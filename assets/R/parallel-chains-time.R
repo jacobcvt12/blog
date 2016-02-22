9d459bf4934374116d92c4a181d410d29ab54d81
Jacob Carey <jacobcvt12@gmail.com>
2016-02-22 11:10:47 -0500

    More work on parallel chains timing graph

--- assets/R/parallel-chains-time.R
+++ assets/R/parallel-chains-time.R
@@ -1,5 +1,5 @@
 library(microbenchmark)
-library(RcppParallelExample)
+devtools::load_all("~/Code/rcpp-parallel")
 library(dplyr)
 library(ggplot2)
 
@@ -9,17 +9,33 @@ set.seed(1)
 # generate data
 mu.true <- 20
 s2.true <- 9
-data <- rnorm(5000, mu.true, sqrt(s2.true))
+
+# empty timing dataset
+timing.data <- data_frame()
 
 # run gibbs sampler
 suppressMessages(
+for (n in c(100, 1000, 10000, 100000)) {
+data <- rnorm(n, mu.true, sqrt(s2.true))
 time <- microbenchmark(
     normal.gibbs(data, chains=1),
     normal.gibbs(data, chains=2),
     normal.gibbs(data, chains=3),
     normal.gibbs(data, chains=4)
 )
+timing.data <- as.data.frame(time) %>%
+    mutate(`length(y)`=n) %>%
+    bind_rows(timing.data)
+}
 )
 
-data <- as.data.frame(time) %>%
-    mutate(time=time/1e6) # time in milliseconds
+data <- timing.data %>%
+    mutate(time=time/1e6, # time in milliseconds
+           expr.char=as.character(expr),
+           chains=substr(expr.char, nchar(expr.char)-1, 
+                         nchar(expr.char)-1)) %>%
+    select(-expr, -expr.char)
+
+ggplot(data, aes(x=chains, y=time)) +
+    geom_violin() +
+    facet_wrap(~`length(y)`, scales="free_y")
