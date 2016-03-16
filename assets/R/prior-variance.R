88262ad5a880be5649cdcc31a859eaa870451689
Jacob Carey <jacobcvt12@gmail.com>
2016-03-15 22:44:43 -0400

    Update simulation and change how plot is made

--- assets/R/prior-variance.R
+++ assets/R/prior-variance.R
@@ -4,15 +4,35 @@ n.i <- 100
 
 ## simple hierarchical model
 mu <- 15
-s.y2 <- 5
+s.y2 <- 1
 s.a2 <- 20
 n.j <- c(3, 10, 25)
 
-y.ij <- lapply(n.j, function(n) { rnorm(n.i * n, 
-                                        mu + rnorm(n, 0, sqrt(s.a2)),
-                                        sqrt(s.y2)) })
-y <- y.ij[[1]]
-g <- rep(1:3, n.i)
-d=data.frame(y, g)
-summarise(group_by(d, g), mean=mean(y))
-plot(density(y))
+alpha.j <- lapply(n.j, function(n) rnorm(n, 0, sqrt(s.a2)))
+y.ij <- lapply(alpha.j, function(alpha) { rnorm(n.i * length(alpha), 
+                                                mu + alpha,
+                                                sqrt(s.y2)) })
+
+library(dplyr)
+data <- bind_rows(data_frame(y=y.ij[[1]], 
+                             g=rep(seq_len(n.j[1]), n.i),
+                             mu=rep(mu + alpha.j[[1]], n.i),
+                             n=n.j[1]),
+                  data_frame(y=y.ij[[2]], 
+                             g=rep(seq_len(n.j[2]), n.i),
+                             mu=rep(mu + alpha.j[[2]], n.i),
+                             n=n.j[2]),
+                  data_frame(y=y.ij[[3]],
+                             g=rep(seq_len(n.j[3]), n.i),
+                             mu=rep(mu + alpha.j[[3]], n.i),
+                             n=n.j[3])) %>%
+    mutate(n=factor(n, levels=n.j))
+
+library(ggplot2)
+ggplot(data, aes(y)) +
+    geom_density(aes(colour=n)) +
+    geom_rug(aes(x=mu)) +
+    facet_wrap(~n) +
+    ylab("") +
+    guides(colour=FALSE) +
+    theme_classic()
