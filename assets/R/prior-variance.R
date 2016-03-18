52739809ca9ddc3ab9090451d751b37bc9b68182
Jacob Carey <jacobcvt12@gmail.com>
2016-03-18 17:23:58 -0400

    Make plot wider

--- assets/R/prior-variance.R
+++ assets/R/prior-variance.R
@@ -29,7 +29,7 @@ data <- bind_rows(data_frame(y=y.ij[[1]],
     mutate(n=factor(n, levels=n.j))
 
 library(ggplot2)
-svg("../img/variance-sim-1.svg", width=5, height=5)
+svg("../img/variance-sim-1.svg", width=8, height=5)
 ggplot(data, aes(y)) +
     geom_density(aes(colour=n)) +
     geom_rug(aes(x=mu)) +
